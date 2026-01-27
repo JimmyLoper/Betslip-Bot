777da72349fec026b0d54a07bffb0940df0a9d19
Dev-Noodle <james.k.loper@gmail.com>
2026-01-27 17:20:19 -0500

    fixed pool require in admin

--- commands/admin.js
+++ commands/admin.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { v4: uuidv4 } = require('uuid');
-const pool = require('../db');
+const pool = require('../utils/db');
 
 module.exports = {
     data: new SlashCommandBuilder()
